69a60acbb523f5c4572b770c0830c8c50974eead
alberto-gvf <81753169+alberto-gvf@users.noreply.github.com>
2024-05-16 21:34:21 +0200

    fix transformer

--- src/transformer.py
+++ src/transformer.py
@@ -2,7 +2,7 @@
 # Transformation: split beween launches and cores tables
 # Write parquet files for each table
 import json
-
+import gc
 from utils import PATH_RESOURCES_RAW, PATH_RESOURCES_STAGE
 import pandas as pd
 
@@ -40,6 +40,7 @@ def add_date_partition_column(pdf):
 
 def save_parquet(pdf, path):
     pdf.to_parquet(path, partition_cols=["p_creation_date"], existing_data_behavior='delete_matching')
+    gc.collect() # needed otherwise it'll hang after writing
 
 
 def get_cores(data):
@@ -50,19 +51,23 @@ def get_cores(data):
             core["date_utc"] = datapoint.get("date_utc")
             if core["core"]:
                 cores.append(core)
-    cores_pdf = pd.DataFrame(cores)
-    cores_pdf = cores_pdf.rename(columns={'core': 'core_id'})
-    cores_pdf = add_date_partition_column(cores_pdf)
-    cores_pdf = cores_pdf.drop(columns=['date_utc'])
+    cores_pdf = (
+        pd.DataFrame(cores)
+        .rename(columns={'core': 'core_id'})
+        .pipe(add_date_partition_column)
+        .drop(columns=['date_utc'])
+    )
     return cores_pdf
 
 
 def get_launches(data):
     launches_pdf = pd.DataFrame(data)
-    launches_pdf["launch_id"] = launches_pdf["id"]
-    launches_pdf = launches_pdf[launches_pdf["launch_id"].notnull()]
-    launches_pdf = launches_pdf.drop_duplicates(subset=["launch_id"], keep='last')
-    launches_pdf = add_date_partition_column(launches_pdf)
+    launches_pdf = (
+        launches_pdf[launches_pdf["id"].notnull()]
+        .rename(columns={'id': 'launch_id'})
+        .pipe(add_date_partition_column)
+        .drop_duplicates(subset=["launch_id"], keep='last')
+    )
     return launches_pdf[LAUNCHES_COLUMNS]
 
 
@@ -75,8 +80,10 @@ def main():
 
     cores_df = get_cores(data)
     save_parquet(cores_df, f"{PATH_RESOURCES_STAGE}/cores")
+    print("cores data saved to parquet table")
     launches_df = get_launches(data)
     save_parquet(launches_df, f"{PATH_RESOURCES_STAGE}/launches")
+    print("launches data saved to parquet table")
 
 
 if __name__ == '__main__':
